75ce8418567b0c6fe1b9efc8fce5bf419a2cf738
Roman Krasnoperov <krasnoperov_rom@mail.ru>
2022-07-27 16:46:18 +0300

    новые изображения для тестов

--- src/main.c
+++ src/main.c
@@ -18,6 +18,17 @@ extern uint16_t test_long_disp_green[512];
 extern uint16_t test_long_disp_blue[512];
 extern uint16_t test_long_disp_black[512];
 extern uint16_t hello[512];
+extern uint16_t zaneprazdneny [512];
+extern uint16_t one_h [512];
+extern uint16_t busy [512];
+extern uint16_t zanjat [512];
+extern uint16_t hujnja [512];
+extern uint16_t svoboden [512];
+extern uint16_t brest [512];
+extern uint16_t vashe_hujnja [512];
+extern uint16_t vashe_hujnja_2 [512];
+extern uint16_t zanjat_2 [512];
+extern uint16_t pivo [512];
 
 int pulse = 100;
 
@@ -26,8 +37,12 @@ int btn = 0;
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     gpio_intr_disable(INPUT_GPIO);
-    btn++;// = !btn;
-    if (btn == 3) btn=0;
+    btn++;
+    if (btn == 12)
+    {
+        btn=0;
+        gpio_intr_enable(INPUT_GPIO);
+    } 
    // gpio_intr_enable(INPUT_GPIO);
 }
 
@@ -51,11 +66,92 @@ void app_main()
 
 
     rmt_tx_init();
-    mbi_set_frame(hello, hello, hello);
+    //mbi_set_frame(zaneprazdneny, test_long_disp_black, test_long_disp_black);
 
     while (1)
     {
-       
+        if (btn== 0)
+        {
+            mbi_set_frame(hello, hello, hello);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==0){}
+        }
+        if (btn== 1)
+        {
+            mbi_set_frame(zaneprazdneny, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==1){}
+        }
+
+        if (btn== 2)
+        {
+            mbi_set_frame(one_h, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==2){}
+        }
+
+         if (btn== 3)
+        {
+            mbi_set_frame(busy, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==3){}
+        }
+
+        if (btn== 4)
+        {
+            mbi_set_frame(zanjat, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==4){}
+        }   
+
+        if (btn== 5)
+        {
+            mbi_set_frame(hujnja, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==5){}
+        }
+
+        if (btn== 6)
+        {
+            mbi_set_frame(test_long_disp_black, svoboden, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==6){}
+        }
+
+        if (btn== 7)
+        {
+            mbi_set_frame(brest, brest, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==7){}
+        }
+
+        if (btn== 8)
+        {
+            mbi_set_frame(vashe_hujnja, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==8){}
+        }
+
+        if (btn== 9)
+        {
+            mbi_set_frame(vashe_hujnja_2, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==9){}
+        }
+
+        if (btn== 10)
+        {
+            mbi_set_frame(zanjat_2, test_long_disp_black, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==10){}
+        }
+
+        if (btn== 11)
+        {
+            mbi_set_frame(pivo, pivo, test_long_disp_black);
+            gpio_intr_enable(INPUT_GPIO);
+            while (btn==11){}
+        }
     }
 
 }
